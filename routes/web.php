1d630158a51ba9f7798b8d5c3110fddc8c0971cf
Gwenael Florian Galliot <g.f.galliot@edu.cegepgarneau.ca>
2025-07-29 21:46:20 -0400

    galerie filter

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::statamic('example', 'example-view', [
-//    'title' => 'Example'
-// ]);
 Route::statamic('Nudes', 'nudes', []);
 Route::statamic('Portraits', 'portraits', []);
 Route::statamic('Argentique', 'argentique', []);
