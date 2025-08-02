425935e2ae30654a5c11cc7e6e1813bf07128d3b
Gwenael Florian Galliot <g.f.galliot@edu.cegepgarneau.ca>
2025-08-01 21:33:50 -0400

    list homepage and shootings

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::statamic('Nudes', 'nudes', []);
-Route::statamic('Portraits', 'portraits', []);
-Route::statamic('Argentique', 'argentique', []);
+Route::statamic('Nu', 'nudes', []);
+Route::statamic('Portrait', 'portraits', []);
+Route::statamic('Shootings', 'shootings', []);
+Route::statamic('Shooting', 'shooting', []);
