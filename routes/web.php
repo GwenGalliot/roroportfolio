5b01774ae91e9b55cc2abc66c0d0ba4d925e34a4
Gwenael Florian Galliot <g.f.galliot@edu.cegepgarneau.ca>
2025-08-02 19:40:28 -0400

    moche mais fonctionne

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::statamic('Nu', 'nudes', []);
 Route::statamic('Portrait', 'portraits', []);
 Route::statamic('Shootings', 'shootings', []);
-Route::statamic('Shooting', 'shooting', []);
+Route::statamic('Shooting/{slug}','shooting');
